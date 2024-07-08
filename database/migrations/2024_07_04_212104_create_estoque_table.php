11bba02ae9a5091c3c3fa6a204488172805a08e5
GustavoSilvaAguiar <gustavogo109@gmail.com>
2024-07-08 14:29:12 -0300

    Mudanças nos arquivos de migration

--- database/migrations/2024_07_04_212104_create_estoque_table.php
+++ database/migrations/2024_07_04_212104_create_estoque_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -11,11 +12,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('estoque', function (Blueprint $table) {
+        /* Schema::create('estoque', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('qtd_estoque');
             $table->string('lcz_estoque', 70);
-            $table->date('data')->nullable()->default('CURRENT_DATE');
+            $table->date('data')->nullable()->default(CURRENT_DATE);
             $table->integer('idproduto');
             $table->integer('idfornecedor')->nullable();
             $table->date('data_validade');
@@ -23,7 +24,22 @@ return new class extends Migration
             $table->enum('tipo_operacao', ['entrada', 'saída'])->nullable();
             $table->integer('idusuario');
             $table->string('observacao')->nullable();
+        }); */
+
+        Schema::create('estoque', function (Blueprint $table) {
+            $table->id();
+            $table->integer('qtd_estoque');
+            $table->string('lcz_estoque', 70);
+            $table->date('data')->nullable()->default(DB::raw('CURRENT_DATE'));
+            $table->integer('idproduto');
+            $table->integer('idfornecedor')->nullable();
+            $table->date('data_validade');
+            $table->string('lote', 50);
+            $table->enum('tipo_operacao', ['entrada', 'saída'])->nullable();
+            $table->integer('idusuario');
+            $table->string('observacao', 255)->nullable();
         });
+        
     }
 
     /**
--- database/migrations/2024_07_04_212104_create_produtos_table.php
+++ database/migrations/2024_07_04_212104_create_produtos_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -11,7 +12,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('produtos', function (Blueprint $table) {
+        /* Schema::create('produtos', function (Blueprint $table) {
             $table->increments('id');
             $table->string('nome', 70);
             $table->text('descricao');
@@ -22,7 +23,22 @@ return new class extends Migration
             $table->integer('idusuario');
             $table->integer('idcategoria');
             $table->integer('idmarca');
+        }); */
+
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome', 70);
+            $table->text('descricao');
+            $table->date('data_cadastro')->nullable()->default(DB::raw('CURRENT_DATE'));
+            $table->string('und_medida', 15);
+            $table->decimal('preco_custo', 10, 2);
+            $table->decimal('preco_venda', 10, 2);
+            $table->integer('idusuario');
+            $table->integer('idcategoria');
+            $table->integer('idmarca');
         });
+        
+        
     }
 
     /**
